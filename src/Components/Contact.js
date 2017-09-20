3e20d511fdf9d88f1cd7c997b0ba5dd3ddbb681d
Matt Pitts <mattpittsmusic@gmail.com>
2017-09-20 14:21:03 -0600

    added contact section

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,8 +4,19 @@ const Contact = () => {
 	return (
 		<div className="contact blue-gradient">
 			<h6 className="section-header">Reach Out</h6>
-			<div>
-				
+			<div className='contact-container'>
+				<div className='icon-text-container'>
+					<i className="fa fa-linkedin" aria-hidden="true"></i>
+					<a href="https://www.linkedin.com/in/matt-pitts/"><p>/matt-pitts</p></a>
+				</div>
+				<div className='icon-text-container'>
+					<i className="fa fa-github" aria-hidden="true"></i>
+					<a href="https://github.com/mattpitts/"><p>/mattpitts</p></a>
+				</div>
+				<div className='icon-text-container'>
+					<i className="fa fa-envelope" aria-hidden="true"></i>
+					<p>mattpittsmusic@gmail.com</p>
+				</div>
 			</div>
 		</div>
 	)
--- src/three-background.js
+++ src/three-background.js
@@ -33,53 +33,6 @@ export default function createBackground() {
 	background.outerBox.recieveShadow = true;
 	background.scene.add(background.outerBox);
 
-	// let manager = new THREE.LoadingManager();
-	// manager.onProgress = function ( item, loaded, total ) {
-	// 	let percent = (loaded / total) * 100;
-	// 	console.log(`Texture loading ${percent}% done.`);
-	// }
-	// manager.onLoad = () => {
-	// 	console.log("Textures Loaded");
-	// 	initScene(textures);
-	// }
-	// let plaster-texture = new THREE.TextureLoader(manager).load('images/plaster-texture.jpg');
-	// //Add a sphere
-	// geometry = new THREE.SphereGeometry(0.1, 0.1, 128);
-	// material = 	new THREE.MeshLambertMaterial({
-	// 	color: 0x46494C
-	// });
-	// background.sphereOne = new THREE.Mesh(geometry, material);
-	// background.sphereOne.position.set(-0.4, -0.25, -.2)
-	// background.scene.add(background.sphereOne);
-
-	// geometry = new THREE.SphereGeometry(0.2, 0.2, 128);
-	// material = 	new THREE.MeshLambertMaterial({
-	// 	color: 0x46494C
-	// });
-	// background.sphereOne = new THREE.Mesh(geometry, material);
-	// background.sphereOne.position.set(0, -.15, 0)
-	// background.scene.add(background.sphereOne);
-
-	// geometry = new THREE.TetrahedronGeometry(.3);
-	// material = 	new THREE.MeshLambertMaterial({
-	// 	color: 0x46494C
-	// });
-	//
-	// background.tetrahedron = new THREE.Mesh(geometry, material);
-	// background.tetrahedron.position.set(0, -.2, 0)
-	// background.scene.add(background.tetrahedron);
-	// background.tetrahedron.rotation.z = THREE.Math.degToRad( 45 );
-	// background.tetrahedron.rotation.x = THREE.Math.degToRad( 5 );
-	// background.tetrahedron.rotation.y = THREE.Math.degToRad( 45 );
-	// background.tetrahedron.rotation.y = THREE.Math.degToRad( 30 );
-
-	// geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.3, 128);
-	// material = 	new THREE.MeshLambertMaterial({
-	// 	color: 0x46494C
-	// });
-	// background.sphereTwo = new THREE.Mesh(geometry, material);
-	// background.sphereTwo.position.set(0, -0.25, -.2)
-	// background.scene.add(background.sphereTwo);
 
 	window.addEventListener('resize', () => {
 		let newWIDTH = window.innerWidth;
